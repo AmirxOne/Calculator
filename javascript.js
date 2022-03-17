851d7a70f65496caef1e617e30ded86af9de70ed
Amir.x.One <Amir.x.one.2000@gmail.com>
2022-03-17 11:22:29 +0330

    add all project files

---
+++ javascript.js
@@ -0,0 +1,137 @@
+// let answer = document.querySelector(".answer")
+// const numberPrint = document.querySelectorAll(".number-print");
+//
+// function clickNumber(event) {
+//     const x = event.target.innerText
+//     if (answer.innerHTML == 0){
+//         return answer.innerHTML = x;
+//     }else {
+//         return answer.innerHTML+= x;
+//     }
+// }
+// numberPrint.forEach(item => {
+//     item.addEventListener("click",clickNumber)
+// })
+// let equal = document.querySelector(".equal");
+// let allClear = document.querySelector(".ac-clear");
+// let oneClear = document.querySelector(".c-clear");
+// equal.addEventListener("click",numberequal)
+// function numberequal() {
+//     let result = answer.innerHTML
+//     answer.innerHTML = eval(result)
+// }
+// allClear.addEventListener("click", clearall)
+// function clearall() {
+//     answer.innerHTML = 0
+// }
+// oneClear.addEventListener("click",clearone)
+// function clearone() {
+//    let text = answer.innerHTML;
+//    if (text.length === 1){
+//        answer.innerHTML = 0
+//    }else {
+//        answer.innerHTML = text.substring(0, text.length-1)
+//    }
+// }
+//
+//
+//
+let answer = document.querySelector(".answer");
+let answerFactor = document.querySelector(".answer-factor")
+let numberPrint = document.querySelectorAll(".number-print");
+numberPrint.forEach(item => {
+    item.addEventListener("click", clickNumber)
+})
+function clickNumber(event) {
+    const x = event.target.innerText
+    if (answer.innerHTML == 0){
+        return answer.innerHTML = x;
+    }else {
+        return answer.innerHTML+= x;
+    }
+}
+let equal = document.querySelector(".equal");
+let allClear = document.querySelector(".ac-clear");
+let oneClear = document.querySelector(".c-clear");
+equal.addEventListener("click", numberequal)
+function numberequal() {
+    const equaltext = answer.innerHTML;
+    answerFactor.innerHTML = equaltext
+    answer.innerHTML = eval(equaltext)
+}
+allClear.addEventListener("click", clearall);
+function clearall() {
+    answer.innerHTML = 0
+    answerFactor.innerHTML = 0
+}
+oneClear.addEventListener("click", clearone);
+function clearone() {
+    let cleartext = answer.innerHTML;
+    if (answer.innerHTML.length === 1){
+        answer.innerHTML = 0
+    }else {
+        answer.innerHTML = cleartext.substring(0, cleartext.length-1)
+        answerFactor.innerHTML = 0
+    }
+}
+const allBoxMode = document.querySelector(".all-box-mode");
+allBoxMode.addEventListener("click", mode)
+function mode() {
+    const divMode = document.querySelector(".mode");
+    const modeColor = document.querySelector(".all-screen");
+    if (divMode.style.right === "17px"){
+        divMode.style.right = "-2px"
+    }else {
+        divMode.style.right = "17px"
+    }
+    if (modeColor.className === "all-screen"){
+        modeColor.className = "all-screen light"
+    }else {
+        modeColor.className = "all-screen"
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
